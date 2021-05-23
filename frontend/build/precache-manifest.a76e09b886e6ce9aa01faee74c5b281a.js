d926a52133c3da278a737ddb4e113ef4e670ee5e
Matan Crispel <matan.cris@gmail.com>
2021-05-23 18:07:54 +0300

    fixing build

---
+++ frontend/build/precache-manifest.a76e09b886e6ce9aa01faee74c5b281a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80d62f4b2ff597f531bc336009e8e5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1735ad0712478401ff09",
+    "url": "/static/css/2.cad97364.chunk.css"
+  },
+  {
+    "revision": "a239e7efa8796b1173f9",
+    "url": "/static/css/main.a04ff026.chunk.css"
+  },
+  {
+    "revision": "1735ad0712478401ff09",
+    "url": "/static/js/2.e478f4a9.chunk.js"
+  },
+  {
+    "revision": "71a03b936f24decbb8262db29c9d258f",
+    "url": "/static/js/2.e478f4a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a239e7efa8796b1173f9",
+    "url": "/static/js/main.1653a03f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "77fa00996ecb4104c7880b8749c7c4e0",
+    "url": "/static/media/Righteous-Regular.77fa0099.ttf"
+  },
+  {
+    "revision": "159cb67fc3bc762a8c3232f0a0c6728e",
+    "url": "/static/media/VarelaRound-Regular.159cb67f.ttf"
+  },
+  {
+    "revision": "6a7fc70a1ce5fd4e495dfd479c8765ad",
+    "url": "/static/media/box.6a7fc70a.png"
+  },
+  {
+    "revision": "d1a782b3c17502381555dbba0d9ffbe9",
+    "url": "/static/media/equalizer5.d1a782b3.gif"
+  },
+  {
+    "revision": "707c51ee1665d87153237d1d7696b289",
+    "url": "/static/media/fxVE.707c51ee.gif"
+  },
+  {
+    "revision": "e3df7b577f61e0415e0f5b6b44ccce7e",
+    "url": "/static/media/hero1.e3df7b57.jpg"
+  },
+  {
+    "revision": "526a0b3788f4ea0f3a66d0a470ddcdf4",
+    "url": "/static/media/hero2.526a0b37.png"
+  },
+  {
+    "revision": "c4b7848553ff6bf6dd94e90fea77c939",
+    "url": "/static/media/hero3.c4b78485.jpg"
+  },
+  {
+    "revision": "6642151dc638424e9cd689fe12479af4",
+    "url": "/static/media/hilla.6642151d.jpg"
+  },
+  {
+    "revision": "37498da733221211ebd842eabd108ab8",
+    "url": "/static/media/idan.37498da7.jpeg"
+  },
+  {
+    "revision": "ce927d4c550bc367159477a5bedea99c",
+    "url": "/static/media/img_placeholder.ce927d4c.png"
+  },
+  {
+    "revision": "2df3552cb4127f60f6b5eb91c7e90b8f",
+    "url": "/static/media/logo.2df3552c.png"
+  },
+  {
+    "revision": "81bc655c2f3862fcb7ef4996c993ccf8",
+    "url": "/static/media/matan.81bc655c.jpg"
+  }
+]);
